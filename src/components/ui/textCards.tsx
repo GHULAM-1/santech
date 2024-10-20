04c46360a4f2ca7000169904aa3ccf0b8acec560
ghulam mohi ud din <GHULAM-1>
2024-10-20 14:59:04 +0500

    update

--- src/components/ui/textCards.tsx
+++ src/components/ui/textCards.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 
 // Define prop types for the TextCard
 interface TextCardProps {
   title: string;
-  description: string;
+  description: string | undefined;
   bgColor?: string;
   socialIcons?: React.ReactNode[]; // Optional prop for social icons
 }
@@ -19,13 +19,13 @@ const TextCard: React.FC<TextCardProps> = ({
   return (
     <div
       className={`p-6 rounded-lg shadow-md ${
-        bgColor ? bgColor : 'bg-green-100'
+        bgColor ? bgColor : "bg-green-100"
       } h-auto flex flex-col justify-between`}
     >
       <div>
         <h3 className="text-xl font-semibold text-gray-800 mb-4">{title}</h3>
         <div className="text-gray-600 text-md">
-          {description.split('\n').map((line, index) => (
+          {description?.split("\n").map((line, index) => (
             <p key={index}>{line}</p>
           ))}
         </div>
